92f4aeb60355dd1df179f3d578dd050ed4514dad
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-06-15 16:37:29 +0530

    changes

--- features/calculator/Result.js
+++ features/calculator/Result.js
@@ -7,11 +7,12 @@ import { CheckBox } from 'react-native-elements';
 import { Picker } from '@react-native-picker/picker';
 // import CheckBox from 'react-native-check-box';
 import { useNavigation } from '@react-navigation/native';
-import { selectCalculator, selectCalculatorError } from './calculatorSlice';
+import { selectCalculator, selectCalculatorError, selectUserInfo } from './calculatorSlice';
 import { useSelector, useDispatch } from 'react-redux';
 
 const App = () => {
   const [isChecked, setIsChecked] = useState(false);
+  const userInfo = useSelector(selectUserInfo);
   const navigation = useNavigation();    
         const handleCheckBox = () => {
           setIsChecked(!isChecked);
@@ -25,7 +26,7 @@ const App = () => {
   <View className=" h-[100%] ">
     <ImageBackground source={require("../../assets/images/bg4.jpg")} resizeMode="cover" className="h-[100%] flex items-center">
       <View className="w-[105%] h-[13%] bg-cyan-200 rounded-b-[100px] flex-row">
-        <Text className="mt-[40px] text-2xl ml-[120px]">Hello, Name</Text>
+        <Text className="mt-[40px] text-2xl ml-[120px]">Hello, {userInfo?userInfo.userName:"Name"}</Text>
         <View className="mt-[40px] ml-[60px] flex items-center justify-center h-[40px] w-[40px] bg-white rounded-3xl">
         <FontAwesome name="user-o" size={24} color="black" /></View>
       </View>
